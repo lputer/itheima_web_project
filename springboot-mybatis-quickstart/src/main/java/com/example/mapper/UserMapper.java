d8f3fdce8d2bf6f6cea1adc534779851993497a8
MinValue <1622022466@qq.com>
2023-05-30 11:23:07 +0800

    01

---
+++ springboot-mybatis-quickstart/src/main/java/com/example/mapper/UserMapper.java
@@ -0,0 +1,14 @@
+package com.example.mapper;
+
+import com.example.pojo.User;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Select;
+
+import java.util.List;
+
+@Mapper // 在运行时,会自动生成该接口的实现类对象,并且将该对象交给IOC容器管理
+public interface UserMapper {
+    // 查询全部的用户信息
+    @Select("select * from user")
+    public List<User> list();
+}
